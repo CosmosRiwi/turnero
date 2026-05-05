fea9463530266486af6795d2edda81f3ebd5b65c
joserodriguez18 <jmra0226@gmail.com>
2026-05-04 21:32:53 -0500

    Interconectar PC's en la misma red

--- Data/querys.sql
+++ Data/querys.sql
@@ -69,5 +69,8 @@ INSERT INTO priorities (name) VALUES ('Normal'), ('Prioritario'), ('VIP');
 -- Comando para ejecutar la migración del database firts
 dotnet ef dbcontext scaffold "Server=157.180.40.190;Database=sistema_turnos;Uid=root;Pwd=CosmosGalaxy;" Pomelo.EntityFrameworkCore.MySql -o Models --context MysqlDbContext --context-dir Data --force
        
+       -- Comandos para los permisos de la impresora en ubuntu 
        sudo chown $USER:$USER /dev/usb/lp0
        sudo chmod 666 /dev/usb/lp0
+       
+       
